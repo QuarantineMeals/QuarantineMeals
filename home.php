789a433a68f9556e1b4f6a661ce39371a2f657d9
HariniMurali06 <54319804+HariniMurali06@users.noreply.github.com>
2021-05-27 19:25:27 +0530

    Added user home page

--- home.php
+++ home.php
@@ -2,3 +2,286 @@
 
 
 ?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quarantine Meals</title>
+
+    <link rel="stylesheet" href="css/user_home.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <!-- Slider Scripts -->
+    <link type="text/css" rel="stylesheet" href="css/lightslider.css" />                  
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/lightslider.js"></script>
+
+    <!--using FontAwesome--------------->
+    <script src="https://kit.fontawesome.com/c8e4d183c2.js" crossorigin="anonymous"></script>
+    
+    
+
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="nav-wrapper ">
+                <img src="img/txtlogo.png" class="brand-logo responsive-img" alt="logo">
+                <ul class="right valign-wrapper" style="font-family: 'Merienda', cursive;">
+                    <li><a href="#">My City</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home-Chefs</a></li>
+                    <li><a href="#">My Cart</a></li>
+                    <li><a href="#">Logout</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <!-- Card Slider Section -->
+    <section id="main">
+        <h1 class="showcase-heading">See What's Cooking...</h1>
+        <ul id="lightSlider">
+            <li class="item-a">
+                <div class="showcase-box">
+                    <img src="img/bgimg.jpg"/>
+                </div>
+            </li>
+            <li class="item-b">
+                <div class="showcase-box">
+                    <img src="img/bgimg.jpg"/>
+                </div>
+            </li>
+            <li class="item-c">
+                <div class="showcase-box">
+                    <img src="img/bgimg.jpg"/>
+                </div>
+            </li>
+            <li class="item-d">
+                <div class="showcase-box">
+                    <img src="img/bgimg.jpg"/>
+                </div>
+            </li>
+            <li class="item-e">
+                <div class="showcase-box">
+                    <img src="img/bgimg.jpg"/>
+                </div>
+            </li>
+        </ul>
+    </section>
+
+    <!-- Popular Foods -->
+    <section id="popular-foods">
+        <h3 class="center">Popular Dishes Of The Month</h3>
+        <p class="center">Don't miss out these amazing mouth-watering dishes</p>
+
+        <div class="row">
+            <div class="container">
+    
+            <div class="col l4 ">
+              <div class="card">
+                <div class="card-image">
+                  <img src="img/bgimg.jpg">
+                </div>
+                <div class="card-content">
+                    <span class="card-title">Card Title</span>
+                  <p>I am a very simple card. I am good at containing small bits of information.
+                  I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                  <div class="row"><span class="price">&#8377 100</span>
+                    <a href="#" class="btn orange right valign-wrapper">Order Now</a></div>
+                </div>
+              </div>
+            </div>
+    
+            <div class="col l4 ">
+              <div class="card">
+                <div class="card-image">
+                  <img src="img/bgimg.jpg">
+                </div>
+                <div class="card-content">
+                    <span class="card-title">Card Title</span>
+                  <p>I am a very simple card. I am good at containing small bits of information.
+                  I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                  <div class="row"><span class="price">&#8377 100</span>
+                    <a href="#" class="btn orange right valign-wrapper">Order Now</a></div>
+                </div>
+              </div>
+            </div>
+    
+            <div class="col l4 ">
+              <div class="card">
+                <div class="card-image">
+                  <img src="img/bgimg.jpg">
+                </div>
+                <div class="card-content">
+                    <span class="card-title">Card Title</span>
+                  <p>I am a very simple card. I am good at containing small bits of information.
+                  I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                  <div class="row"><span class="price">&#8377 100</span>
+                    <a href="#" class="btn orange right valign-wrapper">Order Now</a></div>
+                </div>
+              </div>
+            </div>
+    
+            <div class="col l4 ">
+              <div class="card">
+                <div class="card-image">
+                  <img src="img/bgimg.jpg">
+                </div>
+                <div class="card-content">
+                    <span class="card-title">Card Title</span>
+                  <p>I am a very simple card. I am good at containing small bits of information.
+                  I am convenient because I require little markup to use effectively.</p>
+                </div>
+                <div class="card-action">
+                  <div class="row"><span class="price">&#8377 100</span>
+                    <a href="#" class="btn orange right valign-wrapper">Order Now</a></div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col l4 ">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="img/bgimg.jpg">
+                  </div>
+                  <div class="card-content">
+                      <span class="card-title">Card Title</span>
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                    I am convenient because I require little markup to use effectively.</p>
+                  </div>
+                  <div class="card-action">
+                    <div class="row"><span class="price">&#8377 100</span>
+                      <a href="#" class="btn orange right valign-wrapper">Order Now</a></div>
+                  </div>
+                </div>
+              </div>
+      
+              <div class="col l4 ">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="img/bgimg.jpg">
+                  </div>
+                  <div class="card-content">
+                      <span class="card-title">Card Title</span>
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                    I am convenient because I require little markup to use effectively.</p>
+                  </div>
+                  <div class="card-action">
+                    <div class="row"><span class="price">&#8377 100</span>
+                      <a href="#" class="btn orange right valign-wrapper">Order Now</a></div>
+                  </div>
+                </div>
+              </div>
+              <div class="container center">
+                <div class="btn-large orange darken-4">See More</div>
+              </div>
+             
+            </div>
+          </div>
+
+    </section>
+
+    <section class="featuredchefs">
+      <h3 class="center">Featured Home-Chefs</h3>
+      <p class="center">See all the Home-Chefs supplying food in your city</p>
+      <br>
+      <div class="row">
+        <div class="chef-wrap">
+          <div class="chef-wrap-card">
+            <img src="img/bgimg.jpg">
+            <h5>Chef name</h5>
+            <p class="truncate"> I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            <p>Rating</p>
+          </div>
+          <div class="chef-wrap-card">
+            <img src="img/bgimg.jpg">
+            <h5>Chef name</h5>
+            <p class="truncate"> I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            <p>Rating</p>
+          </div>
+          <div class="chef-wrap-card">
+            <img src="img/bgimg.jpg">
+            <h5>Chef name</h5>
+            <p class="truncate"> I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            <p>Rating</p>
+          </div>
+          <div class="chef-wrap-card">
+            <img src="img/bgimg.jpg">
+            <h5>Chef name</h5>
+            <p class="truncate"> I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            <p>Rating</p>
+          </div>
+          <div class="chef-wrap-card">
+            <img src="img/bgimg.jpg">
+            <h5>Chef name</h5>
+            <p class="truncate"> I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            <p>Rating</p>
+          </div>
+          <div class="chef-wrap-card">
+            <img src="img/bgimg.jpg">
+            <h5>Chef name</h5>
+            <p class="truncate"> I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+            <p>Rating</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <!-- start: FOOTER -->
+    <footer class="page-footer grey darken-4">
+      <div class="container">
+        <div class="row">
+          <div class="col l6 s12">
+            <h5 class="white-text">Footer Content</h5>
+            <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+          </div>
+          <div class="col l4 offset-l2 s12">
+            <h5 class="white-text">Links</h5>
+            <ul>
+              <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+              <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+              <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+              <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <div class="container">
+        © 2014 Copyright Text
+        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+        </div>
+      </div>
+    </footer>
+  <!-- end:Footer -->
+    <script type="text/javascript">
+        $(document).ready(function() {
+        $("#lightSlider").lightSlider({
+            speed: 300, //ms'
+            auto: true,
+            loop: true,
+            slideEndAnimation: true,
+            pause: 2000
+        }); 
+        });
+    </script>
+</body>
+
+</html>
